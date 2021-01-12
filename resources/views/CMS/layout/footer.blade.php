b834831bd3b66e9c9d133bfb50bc4b33cd8d818c
akhyaradn <akhyaradn7@gmail.com>
2021-01-13 03:16:27 +0700

    Kerangka layout html CMS

---
+++ resources/views/CMS/layout/footer.blade.php
@@ -0,0 +1,20 @@
+<script src="{{asset('CMS/plugins/bower_components/jquery/dist/jquery.min.js')}}"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="{{asset('CMS/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+<!-- Menu Plugin JavaScript -->
+<script src="{{asset('CMS/plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js')}}"></script>
+<!--slimscroll JavaScript -->
+<script src="{{asset('CMS/js/jquery.slimscroll.js')}}"></script>
+<!--Wave Effects -->
+<script src="{{asset('CMS/js/waves.js')}}"></script>
+<!-- Custom Theme JavaScript -->
+<script src="{{asset('CMS/js/custom.min.js')}}"></script>
+<!-- Custom tab JavaScript -->
+<script src="{{asset('CMS/js/cbpFWTabs.js')}}"></script>
+<script type="text/javascript">
+(function() {
+    [].slice.call(document.querySelectorAll('.sttabs')).forEach(function(el) {
+        new CBPFWTabs(el);
+    });
+})();
+</script>
---
+++ resources/views/CMS/layout/nav-sidebar.blade.php
@@ -0,0 +1,68 @@
+<div class="navbar-default sidebar" role="navigation">
+    <div class="sidebar-nav">
+        <div class="sidebar-head">
+            <h3><span class="fa-fw open-close"><i class="ti-menu hidden-xs"></i><i class="ti-close visible-xs"></i></span> <span class="hide-menu">Navigation</span></h3> </div>
+        <ul class="nav" id="side-menu">
+            <li class="user-pro">
+                <a href="javascript:void(0)" class="waves-effect"><img src="../plugins/images/users/varun.jpg" alt="user-img" class="img-circle"> <span class="hide-menu"> Steve Gection<span class="fa arrow"></span></span>
+                </a>
+                <ul class="nav nav-second-level collapse" aria-expanded="false" style="height: 0px;">
+                    <li><a href="javascript:void(0)"><i class="ti-user"></i> <span class="hide-menu">My Profile</span></a></li>
+                    <li><a href="javascript:void(0)"><i class="ti-wallet"></i> <span class="hide-menu">My Balance</span></a></li>
+                </ul>
+            </li>
+            <li> <a href="javascript:void(0)" class="waves-effect active"><i class="mdi mdi-av-timer fa-fw" data-icon="v"></i> <span class="hide-menu"> Dashboard <span class="fa arrow"></span> <span class="label label-rouded label-inverse pull-right">4</span></span></a>
+                <ul class="nav nav-second-level">
+                    <li> <a href="index.html"><i class=" fa-fw">1</i><span class="hide-menu">Dashboard 1</span></a> </li>
+                </ul>
+            </li>
+            <li> <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-cart-outline fa-fw" data-icon="v"></i> <span class="hide-menu"> eCommerce <span class="fa arrow"></span> </span></a>
+                <ul class="nav nav-second-level">
+                    <li> <a href="products.html"><i class="fa-fw">P</i><span class="hide-menu">Products</span></a> </li>
+                </ul>
+            </li>
+            <li> <a href="javascript:void(0)" class="waves-effect "><i class="mdi mdi-content-copy fa-fw"></i> <span class="hide-menu">Pages<span class="fa arrow"></span><span class="label label-rouded label-warning pull-right">30</span></span></a>
+                <ul class="nav nav-second-level two-li">
+                    <li><a href="starter-page.html"><i class="ti-layout-width-default fa-fw"></i> <span class="hide-menu">Starter Page</span></a></li>
+                    <li><a href="javascript:void(0)" class="waves-effect"><i class="ti-email fa-fw"></i> <span class="hide-menu">Email Templates</span><span class="fa arrow"></span></a>
+                        <ul class="nav nav-third-level">
+                            <li> <a href="../email-templates/basic.html"><i class="fa-fw">B</i> <span class="hide-menu">Basic</span></a></li>
+                        </ul>
+                    </li>
+                    <li><a href="blank.html"><i class="ti-layout-sidebar-left fa-fw"></i> <span class="hide-menu">Blank Page</span></a></li>
+                </ul>
+            </li>
+            <li class="mega-nav"> <a href="javascript:void(0)" class="waves-effect"><i class="mdi mdi-format-color-fill fa-fw"></i> <span class="hide-menu">UI<span class="fa arrow"></span> <span class="label label-rouded label-info pull-right">20</span> </span></a>
+                <ul class="nav nav-second-level">
+                    <li><a href="panels-wells.html"><i data-icon="&#xe026;" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Panels and Wells</span></a></li>
+                    <li><a href="panel-ui-block.html"><i data-icon="&#xe025;" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Panels With BlockUI</span></a></li>
+                </ul>
+            </li>
+            <li class="devider"></li>
+            <li> <a href="javascript:void(0)" class="waves-effect"><i class="mdi mdi-clipboard-text fa-fw"></i> <span class="hide-menu">Forms<span class="fa arrow"></span></span></a>
+                <ul class="nav nav-second-level two-li">
+                    <li><a href="form-basic.html"><i class="fa-fw">B</i><span class="hide-menu">Basic Forms</span></a></li>
+                    <li><a href="form-layout.html"><i class="fa-fw">L</i><span class="hide-menu">Form Layout</span></a></li>
+                </ul>
+            </li>
+            <li> <a href="javascript:void(0)" class="waves-effect"><i class="mdi mdi-table fa-fw"></i> <span class="hide-menu">Tables<span class="fa arrow"></span><span class="label label-rouded label-danger pull-right">9</span></span></a>
+                <ul class="nav nav-second-level">
+                    <li><a href="basic-table.html"><i class="fa-fw">B</i><span class="hide-menu">Basic Tables</span></a></li>
+                    <li><a href="table-layouts.html"><i class="fa-fw">L</i><span class="hide-menu">Table Layouts</span></a></li>
+                </ul>
+            </li>
+            <li class="last-nav"><a href="javascript:void(0)" class="waves-effect"><i class="mdi mdi-apps fa-fw"></i> <span class="hide-menu">Apps<span class="fa arrow"></span></span></a>
+                <ul class="nav nav-second-level">
+                    <li><a href="chat.html"><i class="ti-comments-smiley fa-fw"></i><span class="hide-menu">Chat-message</span></a></li>
+                    <li><a href="javascript:void(0)" class="waves-effect"><i class="ti-desktop fa-fw"></i><span class="hide-menu">Inbox</span><span class="fa arrow"></span></a>
+                        <ul class="nav nav-third-level">
+                            <li> <a href="inbox.html"><i class="ti-email fa-fw"></i><span class="hide-menu">Mail box</span></a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li class="devider"></li>
+            <li> <a href="widgets.html" class="waves-effect"><i  class="mdi mdi-settings fa-fw"></i> <span class="hide-menu">Widgets</span></a> </li>
+        </ul>
+    </div>
+</div>
