a71f49032013ca104b4d79ddf657e8d8a4c0e50a
akhyaradn <akhyaradn7@gmail.com>
2021-01-16 19:45:07 +0700

    News object attribute & method

--- app/Models/News.php
+++ app/Models/News.php
@@ -5,7 +5,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class News extends Model 
 {
-     
+    public $fillable = [
+        'judul',
+        'img',
+        'konten',
+        'id_penulis',
+        'flag_active'
+    ];
+
+    public $table = 'news';
+
+    protected $guarded = ['id'];
+
+    public $timestamp = true;
 }
 
 ?>
