9cb930b4c115a7c74209e20426f7fcace5bbc956
akhyaradn <akhyaradn7@gmail.com>
2021-01-17 23:24:25 +0700

    DB models

--- app/Models/ProductService.php
+++ app/Models/ProductService.php
@@ -5,6 +5,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductService extends Model
 {
-    
+    public $fillable = [
+        'judul',
+        'paragraf_awal',
+        'paragraf_akhir',
+        'flag_active'
+    ];
+
+    public $table = 'product_service';
+
+    protected $guarded = ['id'];
+
+    public $timestamp = true;
 }
 ?>
--- app/Models/ProductServiceArea.php
+++ app/Models/ProductServiceArea.php
@@ -1,10 +1,22 @@
 <?php
-namespace App\Model;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class ProductServiceArea extends Model
 {
-    
+    public $fillable = [
+        'id_parent',
+        'order',
+        'industry',
+        'mining',
+        'shipping'
+    ];
+
+    public $table = 'product_service_area';
+
+    protected $guarded = ['id'];
+
+    public $timestamp = true;
 }
 ?>
