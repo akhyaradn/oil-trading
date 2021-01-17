5c502e92d0998614b1edf069f3accbce23ddf298
akhyaradn <akhyaradn7@gmail.com>
2021-01-17 12:10:14 +0700

    Komponen product pricing

---
+++ resources/views/CMS/include/product-service-pricing.blade.php
@@ -0,0 +1,7 @@
+<div class="form-group">
+    <label class="col-lg-1 control-label" for="example-input-normal">{{$label}}</label>
+    <div class="input-group col-lg-11">
+        <span class="input-group-addon" id="basic-addon1">Rp</span>
+        <input type="text" class="form-control" name="{{$name}}[]">
+    </div>
+</div>
