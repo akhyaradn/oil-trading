adde2028034cdd38a46fdcc21ca1bcc82c4d1707
akhyaradn <akhyaradn7@gmail.com>
2021-01-18 03:50:42 +0700

    Submit dan get data utk ditampilkan

--- app/Http/Controllers/CMS/ProductServiceController.php
+++ app/Http/Controllers/CMS/ProductServiceController.php
@@ -10,6 +10,22 @@ use Log;
 
 class ProductServiceController extends Controller
 {
+    public function formData($id = null, Request $request) {
+        $data = [
+            'area' => null,
+            'product_service' => null
+        ];
+
+        if($id) {
+            $data = [
+                'product_service' => ProductService::where('id', $id)->first()->toArray(),
+                'area' => ProductServiceArea::where('id_parent', $id)->orderBy('order')->get()->toArray()
+            ];
+        }
+
+        return view('CMS.pages.productservice')->with($data);
+    }
+
     public function submitProductService($id = null, Request $request) {
         try {
             $data = [
@@ -22,7 +38,8 @@ class ProductServiceController extends Controller
             $detail = [
                 'industry' => $request->industry,
                 'mining' => $request->mining,
-                'shipping' => $request->shipping
+                'shipping' => $request->shipping,
+                'id_area' => $request->id_area
             ];
 
             if(!$id) {
@@ -30,12 +47,29 @@ class ProductServiceController extends Controller
                 $data['created_at'] = date("Y-m-d H:i:s");
                 $data['updated_at'] = date("Y-m-d H:i:s");
 
-                $id_area = DB::table('product_service')
+                $id = DB::table('product_service')
                             ->insertGetId($data);
 
                 for($i = 0; $i < 4; $i++) {
                     ProductServiceArea::create([
-                        'id_parent' => $id_area,
+                        'id_parent' => $id,
+                        'order' => ((int)$i + 1),
+                        'industry' => $detail['industry'][$i],
+                        'mining' => $detail['mining'][$i],
+                        'shipping' => $detail['shipping'][$i]
+                    ]);
+                }
+            } else {
+                // tambah field created_at & updated_at hanya utk insertgetid
+                $data['updated_at'] = date("Y-m-d H:i:s");
+
+                ProductService::where('id', $id)
+                ->update($data);
+                
+                for($i = 0; $i < 4; $i++) {
+                    ProductServiceArea::where('id', $detail['id_area'][$i])
+                    ->update([
+                        'id_parent' => $id,
                         'order' => ((int)$i + 1),
                         'industry' => $detail['industry'][$i],
                         'mining' => $detail['mining'][$i],
@@ -44,12 +78,11 @@ class ProductServiceController extends Controller
                 }
             }
 
-            return $id_area;
+            return redirect()->route("formProductService", ['id' => $id])->with(['success' => $data['judul'] . ' saved successfully!']);
 
-            // return redirect()->route("")->with('success', $data['judul'] . ' saved successfully!');
         } catch (\Exception $e) {
             Log::error($e);
-            // return redirect()->route("")->with('failed', 'Failed to save '. $data['judul'] .'!');
+            return redirect()->route("formProductService", ['id' => $id])->with(['failed' => 'Failed to save '. $data['judul'] .'!']);
         }
     }
 }
