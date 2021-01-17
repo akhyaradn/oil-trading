66260b3095eaa4f9686dfe4a25387cb4cf5dea81
akhyaradn <akhyaradn7@gmail.com>
2021-01-18 03:49:31 +0700

    Session, kondisi ada/tidak data

--- resources/views/CMS/pages/productservice.blade.php
+++ resources/views/CMS/pages/productservice.blade.php
@@ -7,55 +7,54 @@
 @endsection
 
 @section('content')
+@if(session('success'))
+<div class="alert alert-success alert-dismissable">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button> 
+    {{session('success')}}
+</div>
+@endif
+@if(session('failed'))
+<div class="alert alert-success alert-dismissable">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button> 
+    {{session('failed')}}
+</div>
+@endif
+
 <div class="row">
-    <form method="POST" class="form-horizontal" id="formarea" name="submitproductservice" action="{{route('submitProductService')}}">
+    <form method="POST" class="form-horizontal" id="formarea" name="submitproductservice" action="{{route('submitProductService')}}{{$product_service ? '/'.$product_service['id'] : ''}}">
     <div class="col-sm-10">
         <div class="white-box">
             {{csrf_field()}}
             <div class="row">
                 <div class="col-lg-12">
-                    @include('CMS.include.news-title')
+                    @include('CMS.include.product-service-title', ['product_service' => $product_service])
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-6">
-                    @include('CMS.include.paragraf-textarea', ['name' => 'paragraf_awal', 'label' => 'First paragraph'])
+                    @include('CMS.include.paragraf-textarea', ['name' => 'paragraf_awal', 'label' => 'First paragraph', 'product_service' => $product_service])
                 </div>
                 <div class="col-lg-6">
-                    @include('CMS.include.paragraf-textarea', ['name' => 'paragraf_akhir', 'label' => 'Second paragraph'])
+                    @include('CMS.include.paragraf-textarea', ['name' => 'paragraf_akhir', 'label' => 'Second paragraph',  'product_service' => $product_service])
                 </div>
             </div>
             <div class="row" id="productservicearea">
+                <!-- Loop 4x form area -->
+                @foreach(range(0,3) as $i) 
                 <div class="col-lg-12 wrap-area">
                     <h3 class="box-title m-t-40"></h3>
-                    @include('CMS.include.product-service-pricing', ['name' => 'industry', 'label' => 'Industry'])
-                    @include('CMS.include.product-service-pricing', ['name' => 'mining', 'label' => 'Mining'])
-                    @include('CMS.include.product-service-pricing', ['name' => 'shipping', 'label' => 'Shipping'])
+                    @include('CMS.include.product-service-pricing', ['name' => 'industry', 'label' => 'Industry',  'area' => $area ? $area[$i] : null])
+                    @include('CMS.include.product-service-pricing', ['name' => 'mining', 'label' => 'Mining',  'area' => $area ? $area[$i] : null])
+                    @include('CMS.include.product-service-pricing', ['name' => 'shipping', 'label' => 'Shipping',  'area' => $area ? $area[$i] : null])
+                    <input type="hidden" name="id_area[]" value="{{ $area && $area[$i]['id'] ? $area[$i]['id'] : null}}">
                 </div>
-                <div class="col-lg-12 wrap-area">
-                    <h3 class="box-title m-t-40"></h3>
-                    @include('CMS.include.product-service-pricing', ['name' => 'industry', 'label' => 'Industry'])
-                    @include('CMS.include.product-service-pricing', ['name' => 'mining', 'label' => 'Mining'])
-                    @include('CMS.include.product-service-pricing', ['name' => 'shipping', 'label' => 'Shipping'])
-                </div>
-                <div class="col-lg-12 wrap-area">
-                    <h3 class="box-title m-t-40"></h3>
-                    @include('CMS.include.product-service-pricing', ['name' => 'industry', 'label' => 'Industry'])
-                    @include('CMS.include.product-service-pricing', ['name' => 'mining', 'label' => 'Mining'])
-                    @include('CMS.include.product-service-pricing', ['name' => 'shipping', 'label' => 'Shipping'])
-                </div>
-                <div class="col-lg-12 wrap-area">
-                    <h3 class="box-title m-t-40"></h3>
-                    @include('CMS.include.product-service-pricing', ['name' => 'industry', 'label' => 'Industry'])
-                    @include('CMS.include.product-service-pricing', ['name' => 'mining', 'label' => 'Mining'])
-                    @include('CMS.include.product-service-pricing', ['name' => 'shipping', 'label' => 'Shipping'])
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
     <div class="col-lg-2">
         <div class="white-box">
-            @include('CMS.include.news-draft-publish')  
+            @include('CMS.include.product-service-flag', ['product_service' => $product_service])  
             <br><br>
             <button type="submit" class="btn btn-success waves-effect waves-light m-r-10">Submit</button>
         </div>
@@ -94,11 +93,10 @@ $(document).ready(function(){
         e.preventDefault();
         var warning = '';
         var fields = serializeInput("#formarea");
+        delete fields['id_area[]'];
         fields['industry[]'] = serializeInputArray("input[name='industry[]']");
         fields['mining[]'] = serializeInputArray("input[name='mining[]']");
         fields['shipping[]'] = serializeInputArray("input[name='shipping[]']");
-
-        console.log(fields);
 
         //Cek jika form kosong
         for(i in fields) {
