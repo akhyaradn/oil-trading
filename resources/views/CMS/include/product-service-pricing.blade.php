0503d258d10b2ebb0d0c2af7cd7571ef9ced8d53
akhyaradn <akhyaradn7@gmail.com>
2021-01-17 23:26:05 +0700

    Ignore except number not leading 0

--- resources/views/CMS/include/product-service-pricing.blade.php
+++ resources/views/CMS/include/product-service-pricing.blade.php
@@ -2,6 +2,26 @@
     <label class="col-lg-1 control-label" for="example-input-normal">{{$label}}</label>
     <div class="input-group col-lg-11">
         <span class="input-group-addon" id="basic-addon1">Rp</span>
-        <input type="text" class="form-control" name="{{$name}}[]">
+        <input type="text" class="form-control numeric" name="{{$name}}[]" onkeypress="allowOnlyNumbers(event)">
     </div>
 </div>
+
+@push('script')
+<!-- Only accept numeric on mining, industry, shipping -->
+<script>
+function allowOnlyNumbers(event) {
+    var value = $(event.target).val();
+
+    if (
+        event.key.length === 1 && 
+        !/\d/.test(event.key)
+    ) {
+        event.preventDefault();
+    }
+
+    if(value.length <= 1 && event.key == 0) {
+        event.preventDefault();
+    }
+}
+</script>
+@endpush
