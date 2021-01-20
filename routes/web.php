c4569ea7f22ccf3df52eef5cf25a0ee4ee61f458
akhyaradn <akhyaradn7@gmail.com>
2021-01-21 06:14:51 +0700

    Bbrp route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Product & service
+Route::get('product-service/{id?}', 'App\Http\Controllers\CMS\ProductServiceController@formData')->name("formProductService");
+Route::post('product-service/submit/{id?}', 'App\Http\Controllers\CMS\ProductServiceController@submitProductService')->name("submitProductService");
+
+// News
+Route::get('news/{flag?}', 'App\Http\Controllers\CMS\NewsController@newsList')->name('newsList');
+Route::get('news/detail/{id?}', 'App\Http\Controllers\CMS\NewsController@formData')->name('formNews');
+Route::post('news/submit/{id?}', 'App\Http\Controllers\CMS\NewsController@submitNews')->name('submitNews');
+Route::post('news/delete/{id?}', 'App\Http\Controllers\CMS\NewsController@deleteNews')->name('deleteNews');
+Route::post('datatable-news', 'App\Http\Controllers\CMS\NewsController@datatableNews')->name('datatableNews');
