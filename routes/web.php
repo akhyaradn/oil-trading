f4581d79bb5e38b8f1f9a455e3047d7f1694f08b
akhyaradn <akhyaradn7@gmail.com>
2021-01-30 09:47:44 +0700

    Main route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function(){
+    return view("index2");
+});
+
 // Product & service
 Route::prefix('cms')->group(function(){
     Route::get('product-service/{flag?}', 'App\Http\Controllers\CMS\ProductServiceController@productServiceList')->name('productServiceList');
